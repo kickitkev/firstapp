e4672efe812276861574dbf990909bb0d5bc4acf
Kev Mitchell <kevin@creativek.co.uk>
2021-08-06 18:49:05 +0100

    move component files to src

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, View } from "react-native";
-import Countries from "../firstapp/assets/components/Countries.tsx";
+import Countries from "./src/components/Countries.tsx";
 
 export default function App() {
   return (
